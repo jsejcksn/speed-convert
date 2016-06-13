bde3ea5ce7d0798cc76dca76167c5d331cc4b411
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-06-12 19:14:08 -0500

    Add single unit time conversion

--- speed-convert.js
+++ speed-convert.js
@@ -49,11 +49,80 @@ var length = {
   }
 };
 
-console.log(convert(3, 'm', 'km'));
-console.log(convert(3, 'm', 'km', true));
-console.log(convertToString(3, 'm', 'km', true));
+var time = {
+  ms: {
+    ms: 1,
+    s: 1 / 1000,
+    m: 1 / 1000 / 60,
+    h: 1 / 1000 / 60 / 60,
+    d: 1 / 1000 / 60 / 60 / 24,
+    w: 1 / 1000 / 60 / 60 / 24 / 7,
+    y: 1 / 1000 / 60 / 60 / 24 / 365.25
+  },
+  s: {
+    ms: 1000,
+    s: 1,
+    m: 1 / 60,
+    h: 1 / 60 / 60,
+    d: 1 / 60 / 60 / 24,
+    w: 1 / 60 / 60 / 24 / 7,
+    y: 1 / 60 / 60 / 24 / 365.25
+  },
+  m: {
+    ms: 60 * 1000,
+    s: 60,
+    m: 1,
+    h: 1 / 60,
+    d: 1 / 60 / 24,
+    w: 1 / 60 / 24 / 7,
+    y: 1 / 60 / 24 / 365.25
+  },
+  h: {
+    ms: 60 * 60 * 1000,
+    s: 60 * 60,
+    m: 60,
+    h: 1,
+    d: 1 / 24,
+    w: 1 / 24 / 7,
+    y: 1 / 24 / 365.25
+  },
+  d: {
+    ms: 24 * 60 * 60 * 1000,
+    s: 24 * 60 * 60,
+    m: 24 * 60,
+    h: 24,
+    d: 1,
+    w: 1 / 7,
+    y: 1 / 365.25
+  },
+  w: {
+    ms: 7 * 24 * 60 * 60 * 1000,
+    s: 7 * 24 * 60 * 60,
+    m: 7 * 24 * 60,
+    h: 7 * 24,
+    d: 7,
+    w: 1,
+    y: 1 / 365.25 / 7
+  },
+  y: {
+    ms: 365.25 * 24 * 60 * 60 * 1000,
+    s: 365.25 * 24 * 60 * 60,
+    m: 365.25 * 24 * 60,
+    h: 365.25 * 24,
+    d: 365.25,
+    w: 365.25 / 7,
+    y: 1
+  }
+};
 
-function convert (val, unitIn, unitOut, asArray) {
+console.log(convertLength(3, 'm', 'km'));
+console.log(convertLength(3, 'm', 'km', true));
+console.log(convertLengthToString(3, 'm', 'km', true));
+console.log(convertTime(30, 'm', 's'));
+console.log(convertTime(30, 'm', 's', true));
+console.log(convertTimeToString(30, 'm', 's', true));
+
+function convertLength (val, unitIn, unitOut, asArray) {
   if (typeof val === 'number' && asArray === true) {
     return [val * length[unitIn][unitOut], unitOut, val, unitIn];
   } else if (typeof val === 'number' && unitOut) {
@@ -65,7 +134,24 @@ function convert (val, unitIn, unitOut, asArray) {
   }
 }
 
-function convertToString (val, unitIn, unitOut, asArray) {
-  var valOut = convert(val, unitIn, unitOut, asArray);
+function convertLengthToString (val, unitIn, unitOut, asArray) {
+  var valOut = convertLength(val, unitIn, unitOut, asArray);
+  return valOut[2] + ' ' + valOut[3] + ' = ' + valOut[0] + ' ' + valOut[1];
+}
+
+function convertTime (val, unitIn, unitOut, asArray) {
+  if (typeof val === 'number' && asArray === true) {
+    return [val * time[unitIn][unitOut], unitOut, val, unitIn];
+  } else if (typeof val === 'number' && unitOut) {
+    return val * time[unitIn][unitOut];
+  } else if (val) {
+    return val;
+  } else {
+    return 0;
+  }
+}
+
+function convertTimeToString (val, unitIn, unitOut, asArray) {
+  var valOut = convertTime(val, unitIn, unitOut, asArray);
   return valOut[2] + ' ' + valOut[3] + ' = ' + valOut[0] + ' ' + valOut[1];
 }
