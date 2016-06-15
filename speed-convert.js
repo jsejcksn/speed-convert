c38e507e4a59d44e9c4c5425d38c5888142c3420
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-06-15 17:57:48 -0500

    Add speed conversion function, unit labels
    - Add speed conversion function
    - Add unit labels
    - Change convertToString to be programmatic

--- speed-convert.js
+++ speed-convert.js
@@ -1,5 +1,6 @@
 var length = {
   cm: {
+    name: 'centimeters',
     cm: 1,
     ft: 0.01 / 0.3048,
     km: 0.00001,
@@ -8,6 +9,7 @@ var length = {
     yd: 0.01 / 0.9144
   },
   ft: {
+    name: 'feet',
     cm: 30.48,
     ft: 1,
     km: 0.0003048,
@@ -16,6 +18,7 @@ var length = {
     yd: 1 / 3
   },
   km: {
+    name: 'kilometers',
     cm: 100000,
     ft: 1000 / 0.3048,
     km: 1,
@@ -24,6 +27,7 @@ var length = {
     yd: 1000 / 0.9144
   },
   m: {
+    name: 'meters',
     cm: 100,
     ft: 1 / 0.3048,
     km: 0.001,
@@ -32,6 +36,7 @@ var length = {
     yd: 1 / 0.9144
   },
   mi: {
+    name: 'miles',
     cm: 160934.4,
     ft: 5280,
     km: 1.609344,
@@ -40,6 +45,7 @@ var length = {
     yd: 1760
   },
   yd: {
+    name: 'yards',
     cm: 91.44,
     ft: 3,
     km: 0.0009144,
@@ -51,6 +57,7 @@ var length = {
 
 var time = {
   ms: {
+    name: 'milliseconds',
     ms: 1,
     s: 1 / 1000,
     m: 1 / 1000 / 60,
@@ -60,6 +67,7 @@ var time = {
     y: 1 / 1000 / 60 / 60 / 24 / 365.25
   },
   s: {
+    name: 'seconds',
     ms: 1000,
     s: 1,
     m: 1 / 60,
@@ -69,6 +77,7 @@ var time = {
     y: 1 / 60 / 60 / 24 / 365.25
   },
   m: {
+    name: 'minutes',
     ms: 60 * 1000,
     s: 60,
     m: 1,
@@ -78,6 +87,7 @@ var time = {
     y: 1 / 60 / 24 / 365.25
   },
   h: {
+    name: 'hours',
     ms: 60 * 60 * 1000,
     s: 60 * 60,
     m: 60,
@@ -87,6 +97,7 @@ var time = {
     y: 1 / 24 / 365.25
   },
   d: {
+    name: 'days',
     ms: 24 * 60 * 60 * 1000,
     s: 24 * 60 * 60,
     m: 24 * 60,
@@ -96,6 +107,7 @@ var time = {
     y: 1 / 365.25
   },
   w: {
+    name: 'weeks',
     ms: 7 * 24 * 60 * 60 * 1000,
     s: 7 * 24 * 60 * 60,
     m: 7 * 24 * 60,
@@ -105,6 +117,7 @@ var time = {
     y: 1 / 365.25 / 7
   },
   y: {
+    name: 'years',
     ms: 365.25 * 24 * 60 * 60 * 1000,
     s: 365.25 * 24 * 60 * 60,
     m: 365.25 * 24 * 60,
@@ -115,16 +128,30 @@ var time = {
   }
 };
 
+console.log(convert(12, 'm', 's', 'cm', 'm'));
+console.log(convert(12, 'm', 's', 'cm', 'm', true));
 console.log(convertLength(3, 'm', 'km'));
 console.log(convertLength(3, 'm', 'km', true));
-console.log(convertLengthToString(3, 'm', 'km', true));
+console.log(convertToString(3, 'm', 'km', 'length', true));
 console.log(convertTime(30, 'm', 's'));
 console.log(convertTime(30, 'm', 's', true));
-console.log(convertTimeToString(30, 'm', 's', true));
+console.log(convertToString(30, 'm', 's', 'time', true));
+
+function convert (val, unit1In, unit2In, unit1Out, unit2Out, asArray) {
+  if (typeof val === 'number' && asArray === true) {
+    return [val, unit1In, unit2In, val * length[unit1In][unit1Out] / time[unit2In][unit2Out], unit1Out, unit2Out];
+  } else if (typeof val === 'number' && unit2Out) {
+    return val * length[unit1In][unit1Out] / time[unit2In][unit2Out];
+  } else if (val) {
+    return val;
+  } else {
+    return 0;
+  }
+}
 
 function convertLength (val, unitIn, unitOut, asArray) {
   if (typeof val === 'number' && asArray === true) {
-    return [val * length[unitIn][unitOut], unitOut, val, unitIn];
+    return [val, unitIn, val * length[unitIn][unitOut], unitOut];
   } else if (typeof val === 'number' && unitOut) {
     return val * length[unitIn][unitOut];
   } else if (val) {
@@ -134,14 +161,9 @@ function convertLength (val, unitIn, unitOut, asArray) {
   }
 }
 
-function convertLengthToString (val, unitIn, unitOut, asArray) {
-  var valOut = convertLength(val, unitIn, unitOut, asArray);
-  return valOut[2] + ' ' + valOut[3] + ' = ' + valOut[0] + ' ' + valOut[1];
-}
-
 function convertTime (val, unitIn, unitOut, asArray) {
   if (typeof val === 'number' && asArray === true) {
-    return [val * time[unitIn][unitOut], unitOut, val, unitIn];
+    return [val, unitIn, val * time[unitIn][unitOut], unitOut];
   } else if (typeof val === 'number' && unitOut) {
     return val * time[unitIn][unitOut];
   } else if (val) {
@@ -151,7 +173,17 @@ function convertTime (val, unitIn, unitOut, asArray) {
   }
 }
 
-function convertTimeToString (val, unitIn, unitOut, asArray) {
-  var valOut = convertTime(val, unitIn, unitOut, asArray);
-  return valOut[2] + ' ' + valOut[3] + ' = ' + valOut[0] + ' ' + valOut[1];
+function convertToString (val, unitIn, unitOut, unitType, asArray) {
+  if (typeof val === 'number' && asArray === true) {
+    if (unitType === 'length') {
+      var valOut = convertLength(val, unitIn, unitOut, asArray);
+    } else if (unitType === 'time') {
+      var valOut = convertTime(val, unitIn, unitOut, asArray);
+    } else {
+      console.error('unitType must be "length" or "time".');
+    }
+    return valOut[0] + ' ' + valOut[1] + ' = ' + valOut[2] + ' ' + valOut[3];
+  } else {
+    console.error('All values not specified.');
+  }
 }
