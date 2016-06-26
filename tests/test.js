d3fda9b9e2eb8cd89063d1386d70765075e4c33d
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-06-26 00:57:21 -0500

    Add tests

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,10 @@
-// Nothing yet
+var speedConvert = require('../index.js');
+
+console.log(speedConvert.convert(12, 'm', 's', 'cm', 'm')); // 72000
+console.log(speedConvert.convert(12, 'm', 's', 'cm', 'm', true)); // [ 12, 'm', 's', 72000, 'cm', 'm' ]
+console.log(speedConvert.convertLength(3, 'm', 'km')); // 0.003
+console.log(speedConvert.convertLength(3, 'm', 'km', true)); // [ 3, 'm', 0.003, 'km' ]
+console.log(speedConvert.convertToString(3, 'm', 'km', 'length', true)); // 3 m = 0.003 km
+console.log(speedConvert.convertTime(30, 'm', 's')); // 1800
+console.log(speedConvert.convertTime(30, 'm', 's', true)); // [ 30, 'm', 1800, 's' ]
+console.log(speedConvert.convertToString(30, 'm', 's', 'time', true)); // 30 m = 1800 s
